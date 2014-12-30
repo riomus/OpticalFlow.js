f0d1acf2c520a89bfe8e4d6cc523af5f5647080f
Bartusiak Roman Dawid <roman.bartusiak@nsn.com>
2014-12-30 18:44:58 +0100

    Initial commit;

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/* OpticalFlow.js main */
+
+// Base function.
+var OpticalFlow = function() {
+  // Add functionality here.
+  return true;
+};
+
+
+// Version.
+OpticalFlow.VERSION = '0.0.0';
+
+
+// Export to the root, which is probably `window`.
+root.OpticalFlow = OpticalFlow;
